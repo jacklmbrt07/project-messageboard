cd2dd3f342e30c45d86bee772dc15026db372827
jacklmbrt07 <jacklmbrt07@gmail.com>
2021-02-19 08:30:01 -0800

    wrote schema

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,25 @@
-'use strict';
+"use strict";
+
+const mongodb = require("mongodb");
+const mongoose = require("mongoose");
 
 module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
-  app.route('/api/replies/:board');
+  mongoose.connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  });
 
+  const postSchema = new mongoose.Schema({
+    text: { type: String, required: true },
+    created_on: Date,
+    bumped_on: Date,
+    reported: Boolean,
+    delete_password: String,
+    replies: [String],
+  });
+
+  app.route("/api/threads/:board");
+
+  app.route("/api/replies/:board");
 };
